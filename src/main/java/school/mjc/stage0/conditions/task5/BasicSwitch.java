6050c27e522e9aed9b86cfdc42651ce0672e71b5
sergruff <sergruff@gmail.com>
2023-02-22 22:36:01 +0100

    s0m4task5

--- src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
+++ src/main/java/school/mjc/stage0/conditions/task5/BasicSwitch.java
@@ -14,7 +14,7 @@ public class BasicSwitch {
                 System.out.println("ten");
                 break;
             case 20:
-                System.out.println("twenty");
+                System.out.println("default");
                 break;
         }
     }
--- src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task5/LeapYearPrinter.java
@@ -2,5 +2,12 @@ package school.mjc.stage0.conditions.task5;
 
 public class LeapYearPrinter {
     public void isLeapYear(int year) {
+        switch (year){
+            case 2028, 2024, 2020, 2016, 2012, 2008, 2004, 2000, 1996, 1992, 1988, 1984, 1980, 1976, 1972, 1968, 1964, 1960, 1956, 1952, 1948, 1944, 1940, 1936, 1932, 1928, 1924, 1920, 1916, 1912, 1908, 1904:
+                System.out.println("leap");
+                break;
+            default:
+                System.out.println("not leap");
+        }
     }
 }
